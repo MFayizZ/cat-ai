e0b7bde2f6d8f2c86eb87aa113b847ea875b05c9
nikhiljmenon959-dot <nikhiljmenon959@gmail.com>
2026-01-20 10:27:11 +0530

    Initialize state variables and emotion function
    Added state variables and emotion handling function.

--- hello.py
+++ hello.py
@@ -1,2 +1,16 @@
-print("hello world")
-print("hello")
+import random
+#----------state-----
+trust=5
+hunger=7
+energy=6
+emotion="neutral"
+emotion_timer=0
+is_sleeping=False
+rule=False
+#-------Helpers------
+def set_emotion(e,t=3):
+  global emotion,emotion_timer
+  if emotion_timer>0:
+    emotion_timer-=1
+  else:
+    emotion="neutral"
